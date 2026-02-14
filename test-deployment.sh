41b9030e0b5fb34675b20ba40290fee1d421a599
EMMD474 <emmanueldaliso3@gmail.com>
2026-02-14 21:24:04 +0200

    improved deployment script

--- test-deployment.sh
+++ test-deployment.sh
@@ -10,8 +10,17 @@ curl -X GET http://localhost:3000/api/notes/urgent
 curl -X GET http://localhost:3000/api/notes/important
 
 # Test typescript syntax
-echo Cecking for Typescript syntax errors..
-pnpm build
-
-# if no erros push changes to repo
-# git push origin emmd
+echo Checking for Typescript syntax errors..
+if pnpm build; then
+    echo "Build successful"
+    echo "pushing changes to online repo..."
+    git push origin emmd
+    echo "switching to master branch.."
+    git switch master && git merge emmd
+    echo "pushing changes to master branch"
+    git push origin master
+    echo "swithcing back to emmd branch"
+else
+    echo "there is an error in the build"
+    exit 1
+fi
